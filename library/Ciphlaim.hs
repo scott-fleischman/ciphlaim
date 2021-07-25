bb9c2a5d3fac386c4651a0d8a0bdd96519941dfd
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2021-07-25 07:01:41 -0700

    Experiment with Hebrew and Greek.

--- library/Ciphlaim.hs
+++ library/Ciphlaim.hs
@@ -2,11 +2,27 @@ module Ciphlaim where
 
 import Brick (App(..))
 import qualified Brick
+import Control.Lens.Operators
+import Data.Generics.Labels ()
 import qualified Graphics.Vty as Vty
+import qualified Brick.Widgets.Border as Border
+import qualified Brick.Widgets.Border.Style as Border.Style
 
 main :: IO ()
 main = do
-  let appDraw () = [Brick.str "Yo"]
+  let hebrew = "בְּרֵאשִׁית בָּרָא אֱלֹהִים אֵת הַשָּׁמַיִם וְאֵת הָאָרֶץ׃"
+      greek = "Τοῦ δὲ Ἰησοῦ χριστοῦ ἡ γένεσις οὕτως ἦν."
+      -- Latin Letter Dental Click '\x01C0'
+      -- Cyrillic Letter Palochka	'\x04CF'
+      borderStyleWithEnglish = Border.Style.unicodeRounded & #bsVertical .~ '\x01C0'
+      hebrewAndGreek =
+        Brick.withBorderStyle borderStyleWithEnglish $ Border.border $ Brick.hLimit 50 $
+        Brick.vBox
+          [ Brick.str hebrew
+          , Brick.str greek
+          ]
+
+      appDraw () = [hebrewAndGreek]
       appChooseCursor () [] = Nothing
       appChooseCursor () (x : _) = Just x
       appHandleEvent () (Brick.VtyEvent (Vty.EvKey _ _)) = Brick.halt ()
