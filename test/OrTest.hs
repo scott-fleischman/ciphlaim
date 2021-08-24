e5cac48a508aa2058aa242dca28ae0118aa5608a
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2021-08-24 06:49:53 -0700

    Use combine in OrTest.

--- test/OrTest.hs
+++ test/OrTest.hs
@@ -9,6 +9,7 @@ import Data.Generics.Labels ()
 import Data.Vector (Vector)
 import Data.Vector qualified as Vector
 import GHC.Generics (Generic)
+import Numeric.Natural (Natural)
 import TestCommon
 
 data OrAssoc = OrAssoc
@@ -19,45 +20,69 @@ data OrAssoc = OrAssoc
   }
   deriving stock (Generic, Eq, Show)
 
+combine :: FinSize -> Vector FinSize -> IndexDirection -> Vector (Natural, OrRef) -> Vector OrAssoc
+combine FinSize {size} sizes dir pairs =
+  (\(value, orRef) -> OrAssoc {fin = Fin {size, value}, orRef, sizes, dir}) <$> pairs
+
 orAssocs :: Vector OrAssoc
 orAssocs =
-  [ OrAssoc Fin {size=1, value=0} OrRef {index=0, value=0} [1] LowerIndexFirst
-
-  , OrAssoc Fin {size=2, value=0} OrRef {index=0, value=0} [2] LowerIndexFirst
-  , OrAssoc Fin {size=2, value=1} OrRef {index=0, value=1} [2] LowerIndexFirst
-
-  , OrAssoc Fin {size=5, value=0} OrRef {index=0, value=0} [2,3] LowerIndexFirst
-  , OrAssoc Fin {size=5, value=1} OrRef {index=0, value=1} [2,3] LowerIndexFirst
-  , OrAssoc Fin {size=5, value=2} OrRef {index=1, value=0} [2,3] LowerIndexFirst
-  , OrAssoc Fin {size=5, value=3} OrRef {index=1, value=1} [2,3] LowerIndexFirst
-  , OrAssoc Fin {size=5, value=4} OrRef {index=1, value=2} [2,3] LowerIndexFirst
-
-  , OrAssoc Fin {size=9, value=0} OrRef {index=0, value=0} [4,2,3] LowerIndexFirst
-  , OrAssoc Fin {size=9, value=1} OrRef {index=0, value=1} [4,2,3] LowerIndexFirst
-  , OrAssoc Fin {size=9, value=2} OrRef {index=0, value=2} [4,2,3] LowerIndexFirst
-  , OrAssoc Fin {size=9, value=3} OrRef {index=0, value=3} [4,2,3] LowerIndexFirst
-  , OrAssoc Fin {size=9, value=4} OrRef {index=1, value=0} [4,2,3] LowerIndexFirst
-  , OrAssoc Fin {size=9, value=5} OrRef {index=1, value=1} [4,2,3] LowerIndexFirst
-  , OrAssoc Fin {size=9, value=6} OrRef {index=2, value=0} [4,2,3] LowerIndexFirst
-  , OrAssoc Fin {size=9, value=7} OrRef {index=2, value=1} [4,2,3] LowerIndexFirst
-  , OrAssoc Fin {size=9, value=8} OrRef {index=2, value=2} [4,2,3] LowerIndexFirst
-
-  , OrAssoc Fin {size=5, value=0} OrRef {index=1, value=0} [2,3] HigherIndexFirst
-  , OrAssoc Fin {size=5, value=1} OrRef {index=1, value=1} [2,3] HigherIndexFirst
-  , OrAssoc Fin {size=5, value=2} OrRef {index=1, value=2} [2,3] HigherIndexFirst
-  , OrAssoc Fin {size=5, value=3} OrRef {index=0, value=0} [2,3] HigherIndexFirst
-  , OrAssoc Fin {size=5, value=4} OrRef {index=0, value=1} [2,3] HigherIndexFirst
-
-  , OrAssoc Fin {size=9, value=0} OrRef {index=2, value=0} [4,2,3] HigherIndexFirst
-  , OrAssoc Fin {size=9, value=1} OrRef {index=2, value=1} [4,2,3] HigherIndexFirst
-  , OrAssoc Fin {size=9, value=2} OrRef {index=2, value=2} [4,2,3] HigherIndexFirst
-  , OrAssoc Fin {size=9, value=3} OrRef {index=1, value=0} [4,2,3] HigherIndexFirst
-  , OrAssoc Fin {size=9, value=4} OrRef {index=1, value=1} [4,2,3] HigherIndexFirst
-  , OrAssoc Fin {size=9, value=5} OrRef {index=0, value=0} [4,2,3] HigherIndexFirst
-  , OrAssoc Fin {size=9, value=6} OrRef {index=0, value=1} [4,2,3] HigherIndexFirst
-  , OrAssoc Fin {size=9, value=7} OrRef {index=0, value=2} [4,2,3] HigherIndexFirst
-  , OrAssoc Fin {size=9, value=8} OrRef {index=0, value=3} [4,2,3] HigherIndexFirst
-  ]
+  combine 1 [1] LowerIndexFirst
+    [ (0, OrRef {index=0, value=0})
+    ]
+  <>
+  combine 2 [2] LowerIndexFirst
+    [ (0, OrRef {index=0, value=0})
+    , (1, OrRef {index=0, value=1})
+    ]
+  <>
+  combine 5 [2,3] LowerIndexFirst
+    [ (0, OrRef {index=0, value=0})
+    , (1, OrRef {index=0, value=1})
+    , (2, OrRef {index=1, value=0})
+    , (3, OrRef {index=1, value=1})
+    , (4, OrRef {index=1, value=2})
+    ]
+  <>
+  combine 9 [4,2,3] LowerIndexFirst
+    [ (0, OrRef {index=0, value=0})
+    , (1, OrRef {index=0, value=1})
+    , (2, OrRef {index=0, value=2})
+    , (3, OrRef {index=0, value=3})
+    , (4, OrRef {index=1, value=0})
+    , (5, OrRef {index=1, value=1})
+    , (6, OrRef {index=2, value=0})
+    , (7, OrRef {index=2, value=1})
+    , (8, OrRef {index=2, value=2})
+    ]
+  <>
+  combine 1 [1] HigherIndexFirst
+    [ (0, OrRef {index=0, value=0})
+    ]
+  <>
+  combine 2 [2] HigherIndexFirst
+    [ (0, OrRef {index=0, value=0})
+    , (1, OrRef {index=0, value=1})
+    ]
+  <>
+  combine 5 [2,3] HigherIndexFirst
+    [ (0, OrRef {index=1, value=0})
+    , (1, OrRef {index=1, value=1})
+    , (2, OrRef {index=1, value=2})
+    , (3, OrRef {index=0, value=0})
+    , (4, OrRef {index=0, value=1})
+    ]
+  <>
+  combine 9 [4,2,3] HigherIndexFirst
+    [ (0, OrRef {index=2, value=0})
+    , (1, OrRef {index=2, value=1})
+    , (2, OrRef {index=2, value=2})
+    , (3, OrRef {index=1, value=0})
+    , (4, OrRef {index=1, value=1})
+    , (5, OrRef {index=0, value=0})
+    , (6, OrRef {index=0, value=1})
+    , (7, OrRef {index=0, value=2})
+    , (8, OrRef {index=0, value=3})
+    ]
 
 orTest :: IO ()
 orTest = do
